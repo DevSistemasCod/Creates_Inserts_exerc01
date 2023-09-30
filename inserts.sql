d9008fc8e9f653ed74d6dd5ed5ae20a4c338ab7d
DevSistemasCod <124713140+DevSistemasCod@users.noreply.github.com>
2023-09-30 20:37:34 -0300

    Update inserts.sql

--- inserts.sql
+++ inserts.sql
@@ -4,21 +4,25 @@ VALUES
     ('TI', 'Tecnologia da Informação', 20),
     ('VENDAS', 'Vendas', 15),
     ('ADM', 'Administrativo', 11),
-    ('MKT', 'Marketing', 4);
+    ('MKT', 'Marketing', 4),
+    ('JUR', 'Juridico', 5),
+    ('COM', 'Comercial', 2);
 
-INSERT INTO funcionario (codigo_funcionario, nome_funcionario, cargo, sigla_depto)
+INSERT INTO funcionario (codigo_funcionario, nome_funcionario, cargo, sigla_depto, data_admissao, salario)
 VALUES
-    (100, 'Fernanda Lima', 'Vendas', 'VENDAS'),
-    (101, 'Fabio Santos', 'Desenvolvedor', 'TI'),
-    (102, 'Bianca Costa', 'Desenvolvedora', 'TI'),
-    (103, 'Carlos Oliveira', 'Vendedor', 'VENDAS'),
-    (104, 'Thiago Pereira', 'Contador', 'ADM'),
-    (105, 'Luiza Costa', 'Secretaria', 'RH'),
-    (106, 'João Nascimento', 'Promotor', 'MKT'),
-    (107, 'Ana Alencar', 'Secretaria', 'RH'),
-    (108, 'Vanessa Gomes', 'Contadora', 'ADM'),
-    (109, 'Jonas da  Silva', 'Promotor', 'MKT'),
-    (110, 'Romeu de Nobrega', 'Desenvolvedor', 'TI');
+    (100, 'Fernanda Lima', 'Vendas', 'VENDAS', '2017-09-21', 4000.00),
+    (101, 'Fabio Gomes', 'Desenvolvimento', 'TI', '2019-05-29', 3000.00),
+    (102, 'Bianca Costa', 'Desenvolvimento', 'TI', '2015-01-01', 6500.00),
+    (103, 'Carlos Oliveira', 'Vendas', 'VENDAS', '2020-11-15', 3400.00),
+    (104, 'Thiago Pereira', 'Contabilidade', 'ADM', '2021-07-10', 3600.00),	    
+    (105, 'Luiza Costa', 'Secretaria', 'RH', '2018-02-01', 3000.00 ),
+    (106, 'João Nascimento', 'Promoter', 'MKT', '2018-02-01', 4100.00),
+    (107, 'Ana Alencar', 'Secretaria', 'RH', '2020-03-12', 5200.00),
+    (108, 'Vanessa Gomes', 'Contabilidade', 'ADM', '2020-03-18', 4900.00),
+    (109, 'Jonas da  Silva', 'Promoter', 'MKT','2020-03-02',  3700.00),	    
+    (110, 'Romeu de Nobrega', 'Desenvolvimento', 'TI','2021-04-22', 4600.00),
+    (111, 'Amadeu Gomes da Silva', 'Promoter', 'MKT','2022-04-22', 3600.00 ),
+    (112, 'Gabriel Torres', 'Desenvolvimento', 'TI','2016-01-16', 6800.00);
 
 INSERT INTO projeto (sigla_projeto, nome_projeto, codigo_funcionario, sigla_depto)
 VALUES
@@ -33,4 +37,3 @@ VALUES
     ('ADM02', 'Treinamento de Novos Funcionários', 108, 'ADM'),
     ('MKT02', 'Propaganda de Atacado', 109, 'MKT'),
     ('TI03', 'Atualização de Versão', 110, 'TI');
-    

91ae1f3e9b69e436759b3b252f047b43877967dd
DevSistemasCod <124713140+DevSistemasCod@users.noreply.github.com>
2025-10-08 16:23:04 -0300

    Update creates.sql

--- creates.sql
+++ creates.sql
@@ -1,31 +1,36 @@
 CREATE DATABASE banco_modelo01;
-
 USE banco_modelo01;
 
+-- Tabela de departamentos
 CREATE TABLE departamento (
-    sigla_depto VARCHAR(20),
-    nome_depto VARCHAR(50),
-    qtd_funcionarios_depto INT NOT NULL CHECK(qtd_funcionarios_depto >= 0),
-    CONSTRAINT pk_departamento PRIMARY KEY (sigla_depto, nome_depto)
+    sigla_depto VARCHAR(20) PRIMARY KEY,
+    nome_depto VARCHAR(50) NOT NULL,
+    qtd_funcionarios_depto INT NOT NULL CHECK(qtd_funcionarios_depto >= 0)
 );
 
+-- Tabela de funcionários
 CREATE TABLE funcionario (
-    codigo_funcionario INT,
+    codigo_funcionario INT PRIMARY KEY,
     nome_funcionario VARCHAR(50) NOT NULL,
     cargo VARCHAR(100) NOT NULL,
     sigla_depto VARCHAR(20) NOT NULL,
     data_admissao DATE NOT NULL, 
     salario DECIMAL(10,2) NOT NULL,
-    CONSTRAINT pk_funcionario PRIMARY KEY (codigo_funcionario, nome_funcionario),
-    CONSTRAINT fk_departamento_funcionario FOREIGN KEY (sigla_depto) REFERENCES departamento(sigla_depto) ON DELETE CASCADE ON UPDATE CASCADE
+    CONSTRAINT fk_departamento_funcionario 
+        FOREIGN KEY (sigla_depto) 
+        REFERENCES departamento(sigla_depto) ON DELETE CASCADE ON UPDATE CASCADE
 );
 
+-- Tabela de projetos
 CREATE TABLE projeto (
-    sigla_projeto VARCHAR(20),
-    nome_projeto VARCHAR(50),
+    sigla_projeto VARCHAR(20) PRIMARY KEY,
+    nome_projeto VARCHAR(50) NOT NULL,
     codigo_funcionario INT NOT NULL,
     sigla_depto VARCHAR(20) NOT NULL,
-    CONSTRAINT pk_projeto PRIMARY KEY (sigla_projeto, nome_projeto),
-    CONSTRAINT fk_departamento_projeto FOREIGN KEY (sigla_depto) REFERENCES departamento(sigla_depto) ON DELETE CASCADE ON UPDATE CASCADE,
-    CONSTRAINT fk_funcionario_projeto FOREIGN KEY (codigo_funcionario) REFERENCES funcionario(codigo_funcionario) ON DELETE CASCADE ON UPDATE CASCADE
+    CONSTRAINT fk_departamento_projeto 
+        FOREIGN KEY (sigla_depto) 
+        REFERENCES departamento(sigla_depto) ON DELETE CASCADE ON UPDATE CASCADE,
+    CONSTRAINT fk_funcionario_projeto 
+        FOREIGN KEY (codigo_funcionario) 
+        REFERENCES funcionario(codigo_funcionario) ON DELETE CASCADE ON UPDATE CASCADE
 );
